2c87a36947b6ee53e892b65811fe03d0682b6aef
PaluruSubbaNarasaiah <palurusubbanarasaiah@gmail.com>
2025-06-18 19:20:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import RSVPForm from './components/RSVPForm';
+import MapComponent from './components/MapComponent';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>🎉 Birthday Invitation 🎉</h1>
+      <video width="90%" controls autoPlay loop>
+        <source src="/birthday.mp4" type="video/mp4" />
+      </video>
+      <RSVPForm />
+      <MapComponent />
+    </div>
+  );
+}
+
+export default App;
