863cc88414a7c3a87a439b52f2ceb6468525ec37
PaluruSubbaNarasaiah <palurusubbanarasaiah@gmail.com>
2025-06-18 19:40:35 +0530

    nani

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
+// import React from 'react';
+// import RSVPForm from './components/RSVPForm';
+// import MapComponent from './components/MapComponent';
+
+// function App() {
+//   return (
+//     <div className="container">
+//       <h1>🎉 Birthday Invitation 🎉</h1>
+//       <video width="90%" controls autoPlay loop>
+//         <source src="/birthday.mp4" type="video/mp4" />
+//       </video>
+//       <RSVPForm />
+//       <MapComponent />
+//     </div>
+//   );
+// }
+
+// export default App;
 import React from 'react';
 import RSVPForm from './components/RSVPForm';
 import MapComponent from './components/MapComponent';
 
 function App() {
   return (
-    <div className="container">
+    <div className="container text-center p-4">
       <h1>🎉 Birthday Invitation 🎉</h1>
-      <video width="90%" controls autoPlay loop>
-        <source src="/birthday.mp4" type="video/mp4" />
-      </video>
+
+      {/* Display the image instead of video */}
+      <img
+        src="/birthday report.jpg"
+        alt="Birthday Invitation"
+        style={{ width: '90%', borderRadius: '10px', margin: '20px 0' }}
+      />
+
+      {/* RSVP Form */}
       <RSVPForm />
+
+      {/* Map */}
       <MapComponent />
     </div>
   );
